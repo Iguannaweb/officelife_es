b2abc2d97dd5ecac3aa60b9135169d720ac7f120
crishnakh <crishnakh@gmail.com>
2022-08-20 16:45:49 +0000

    welcome.php translated to es

--- resources/lang/es/welcome.php
+++ resources/lang/es/welcome.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'hide_message_forever' => 'Hide this welcome tab forever',
-    'thanks' => 'Thanks for joining OfficeLife.',
-    'admin_line_1' => 'As you’ve added this company, you are effectively the administrator of this account. You will be able to add employees, edit teams and manage everything in it.',
-    'admin_line_2' => 'However, at this stage, your account is empty. OfficeLife is really useful when everything is properly set up. We know it’s a bit of a pain to do, so we’ve compiled a list of things you should do in this handy guide to make the most out of your account:',
-    'admin_guide_cta' => 'Read our easy, simple setup guide',
+    'hide_message_forever' => 'Oculta este mensaje de bienvenida para siempre',
+    'thanks' => 'Gracias por unirte a OfficeLife.',
+    'admin_line_1' => 'Como eres el creador de la empresa, efectivamente eres el administrador de la misma. Puedes agregar empleados, editar equipos y organizar lo que necesites.',
+    'admin_line_2' => 'Sin embarto, en este momento, tu cuenta está vacía. OfficeLife funciona mejor cuando todo está propiamente condigurado. Sabemos que puede ser un poco tedioso, así que te hemos dejado una lista de cosas a modo de guía para mejorar tu cuenta:',
+    'admin_guide_cta' => 'Lee nuestra sencilla y simple guía de configuración',
 ];
