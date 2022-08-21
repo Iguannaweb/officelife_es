191dbf53ff3a9ca503039d8ee4b073495a63a9c9
crishnakh <crishnakh@gmail.com>
2022-08-21 08:01:28 +0000

    Translated mail.php

--- resources/lang/es/mail.php
+++ resources/lang/es/mail.php
@@ -1,13 +1,13 @@
 <?php
 return [
-    'whoops' => 'Whoops!',
-    'hello' => 'Hello!',
-    'thanks' => 'Thanks',
-    'footer' => 'If you’re having trouble clicking the ":actionText" button, copy and paste the URL below into your web browser:',
-    'confirmation_email_title' => 'Please confirm your email address',
-    'confirmation_email_greetings' => 'Hello :name!',
-    'confirmation_email_intro' => 'Thanks for signing up.',
-    'confirmation_email_action' => 'There is just one more thing to do: we need you to confirm that you really own the email address used to subscribe to OfficeLife. We apologize for having to do this, but… Internet.',
-    'confirmation_email_button' => 'Confirm your email address',
+    'whoops' => '¡Uuppps!',
+    'hello' => '¡Hola!',
+    'thanks' => 'Gracias',
+    'footer' => 'Si estás teniendo problemas pulsa el botón ":actionText", copia y pega la siguiente URL en tu explorador web:',
+    'confirmation_email_title' => 'Por favor, confirma tu dirección de correo',
+    'confirmation_email_greetings' => '¡Hola :name!',
+    'confirmation_email_intro' => 'Gracias por registrarte.',
+    'confirmation_email_action' => 'Solo queda una cosa: confirma que eres el dueño del correo proporcionado para suscribirte a OfficeLife.',
+    'confirmation_email_button' => 'Confirma tu correo electrónico',
     'invite_employee_to_become_user' => 'Create your account on OfficeLife',
 ];
